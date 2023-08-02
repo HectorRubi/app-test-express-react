6f3d95e4f5ec3fb74094d4943544820785a44ce2
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-02 14:12:11 -0600

    Add prettier configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const { env } = require("./config/env");
+const express = require('express');
+const { env } = require('./config/env');
 const app = express();
 const port = env.port;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
