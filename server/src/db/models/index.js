0bb5cfc9463d138ab35bc859e7b9044bf65b0788
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-03 12:54:50 -0600

    Add PokemonAbility model to sequelize

--- server/src/db/models/index.js
+++ server/src/db/models/index.js
@@ -1,10 +1,18 @@
 const { Pokemon, PokemonAttributes } = require('./pokemon.model');
 const { Ability, AbilityAttributes } = require('./ability.model');
+const {
+  PokemonAbility,
+  PokemonAbilityAttributes,
+} = require('./pokemon-ability.model');
 
 class Models {
   static setup(sequelize) {
     Pokemon.init(PokemonAttributes, Pokemon.config(sequelize));
     Ability.init(AbilityAttributes, Ability.config(sequelize));
+    PokemonAbility.init(
+      PokemonAbilityAttributes,
+      PokemonAbility.config(sequelize)
+    );
 
     Pokemon.associate(sequelize.models);
     Ability.associate(sequelize.models);
