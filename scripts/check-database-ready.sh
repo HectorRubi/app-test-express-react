736fceff42b0743b0e945eb88b2556030e4284bc
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-04 15:08:35 -0600

    Add volume for DB data and check if exist in script

--- scripts/check-database-ready.sh
+++ scripts/check-database-ready.sh
@@ -2,11 +2,10 @@
 searching=1
 while [ $searching -eq 1 ]
 do
+  volume=$(docker volume ls  -q | grep -o "app-test-express-react_db_data")
   res=$(docker-compose logs db | grep -o "database system is ready to accept connections" | wc -l)
-  if [[ $res -eq 2 ]]
+  if [ $res -eq 2 ] || [ $volume ]
   then
-    echo $(docker-compose logs db | grep -o "database system is ready to accept connections")
-    echo $(docker-compose logs db | grep -o "database system is ready to accept connections" | wc -l)
     searching=$(( $searching + 1))
   fi
 done
